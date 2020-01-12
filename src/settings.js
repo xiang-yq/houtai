eb8ba90a5a70c7cd978b521857b9aefb5a15de38
xiangyuanqi <965017469@qq.com>
2020-01-12 15:31:53 +0800

    first

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+// 网页标题
+  title: 'Vue Wjl Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 头部固定
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 左边的slide导航增加logo
+   */
+  sidebarLogo: true
+}
